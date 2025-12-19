2122e151d38586b0fcab74a6c28b0800d389842c
ZunainTahir <zunaintahir456@gmail.com>
2025-12-20 01:19:54 +0500

    third commit

--- proj.cpp
+++ proj.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
 struct Player {
     string name;
     int health = 20, maxHealth = 20;
@@ -20,9 +19,9 @@ void displayStats(const Player& p) {
     cout << "HERO: " << p.name << "\n";
     cout << "===============\n";
     cout << "Health: " << p.health << "/" << p.maxHealth << endl;
-    cout << "Strength: " << p.strength << " | Intelligence: " << p.intelligence << endl;
-    cout << " | Dexterity: " << p.dexterity << endl;
-    cout << "Gold: " << p.gold << " | Reputation: " << p.reputation << endl;
+    cout << "Strength: " << p.strength << "|Intelligence: " << p.intelligence << endl;
+    cout << "|Dexterity: " << p.dexterity << endl;
+    cout << "Gold: " << p.gold << " |Reputation: " << p.reputation << endl;
     cout << "Inventory: ";
     bool empty = true;
     for (const string& item : p.inventory) {
@@ -39,10 +38,18 @@ void chooseBackstory(Player& p) {
     int c; 
     cin >> c;
     switch(c) {
-        case 1: p.intelligence++; p.gold += 30; cout << "You were raised in luxury.\n"; break;
-        case 2: p.dexterity++; cout << "You grew up on the harsh streets.\n"; break;
-        case 3: p.strength++; cout << "You worked the fields with iron will.\n"; break;
-        default: cout << "You are a simple peasant.\n"; break;
+        case 1: 
+            p.intelligence++; 
+            p.gold += 30; 
+            cout << "You were raised in luxury.\n"; break;
+        case 2: 
+            p.dexterity++; 
+            cout << "You grew up on the harsh streets.\n"; break;
+        case 3: 
+            p.strength++; 
+            cout << "You worked the fields with iron will.\n"; break;
+        default: 
+            cout << "You are a simple peasant.\n"; break;
     }
 }
 void chooseRace(Player& p) {
@@ -53,10 +60,17 @@ void chooseRace(Player& p) {
     int c; 
     cin >> c;
     switch(c) {
-        case 1: p.intelligence++; cout << "Human ambition burns within.\n"; break;
-        case 2: p.dexterity++; cout << "Elven grace flows through you.\n"; break;
-        case 3: p.strength++; cout << "Dwarven endurance is yours.\n"; break;
-        default: cout << "Your bloodline is unknown...\n"; break;
+        case 1: 
+            p.intelligence++; 
+            cout << "Human ambition burns within.\n"; break;
+        case 2: 
+            p.dexterity++; 
+            cout << "Elven grace flows through you.\n"; break;
+        case 3: 
+            p.strength++; 
+            cout << "Dwarven endurance is yours.\n"; break;
+        default: 
+            cout << "Your bloodline is unknown...\n"; break;
     }
 }
 void chooseClass(Player& p) {
@@ -67,9 +81,27 @@ void chooseClass(Player& p) {
     int c;
     cin >> c;
     switch(c) {
-        case 1: p.strength += 2; cout << "You are a fearsome Warrior!\n"; break;
-        case 2: p.intelligence += 2; cout << "You wield powerful Magic!\n"; break;
-        case 3: p.dexterity += 2; cout << "You are a deadly Rogue!\n"; break;
-        default: cout << "You forge your own destiny.\n"; break;
+        case 1: 
+            p.strength += 2; 
+            cout << "You are a fearsome Warrior!\n"; break;
+        case 2: 
+            p.intelligence += 2; 
+            cout << "You wield powerful Magic!\n"; break;
+        case 3: 
+            p.dexterity += 2; 
+            cout << "You are a deadly Rogue!\n"; break;
+        default: 
+            cout << "You forge your own destiny.\n"; break;
+    }
+    void addItem(Player& p, const string& item, int slot) {
+        if (slot < 10) {
+            p.inventory[slot] = item;
+        }
+    }
+    bool hasItem(const Player& p, int slot) {
+        return slot < 10 && !p.inventory[slot].empty();
+    }
+    void removeItem(Player& p, int slot) {
+        if (slot < 10) p.inventory[slot] = "";
     }
 }
