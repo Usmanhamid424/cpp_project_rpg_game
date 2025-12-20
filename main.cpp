80d93b8ac4db5891ec99e972665406380640a30b
Usman Hamid <uhamid597@gmail.com>
2025-12-20 18:16:59 +0500

    new main

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <fstream>
+using namespace std;
+struct Player {
+    string name;
+    int health;
+    int strength;
+    int intelligence;
+    int dexterity;
+    int rep;
+    int money;
+};
+char gameMap[7][7] = {
+    {'W', '.', '.', ' ', ' ', ' ', 'V'},
+    {'.', '.', ' ', 'B', ' ', '.', '.'},
+    {'.', ' ', '.', '.', '.', ' ', 'E'},
+    {'Q', '.', '.', 'R', '.', '.', 'S'},
+    {' ', '.', ' ', '.', ' ', '.', ' '},
+    {'.', '.', '.', '.', '.', '.', '.'},
+    {'E', '.', 'T', 'F', '.', ' ', ' '}
+};
+void printMap(char map[][7], int rows) {
+    cout << "Game Map:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < 7; j++) {
+            cout << map[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+void displayStats(const Player &player) {
+    cout << "You have " << player.strength << " strength\n";
+    cout << "You have " << player.intelligence << " intelligence\n";
+    cout << "You have " << player.dexterity << " dexterity\n";
+    cout << "You have " << player.health << " health\n";
+}
