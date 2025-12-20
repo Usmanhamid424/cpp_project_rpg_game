5342b9ca92aafc6d9af354a1fa99573288a51b75
ZunainTahir <zunaintahir456@gmail.com>
2025-12-20 21:20:40 +0500

    Adding new function

--- main.cpp
+++ main.cpp
@@ -49,11 +49,130 @@ void increaseStat(int *stat, int amount) {
 }
 int attackThresholds[4][2] = {{92, 10}, {75, 5}, {45, 3}, {0, 2}};
 string attackTypes[4] = {"speical", "heavy", "normal", "light"};
-void printInventory(string inv[], int size) {
-    cout << "Inventory:\n";
-    for (int i = 0; i < size; i++) {
-        if (!inv[i].empty()) {
-            cout << inv[i] << "\n";
+int getPlayerDamage(int fightType) {
+    if (fightType == 1) return 5;
+    else if (fightType == 2) return 3;
+    else if (fightType == 3) return 2;
+    return 0;
+}
+
+// Function to get enemy attack (uses multidimensional array, pass by reference for damage and type)
+void getEnemyAttack(int random, int &damage, string &type) {
+    for (int i = 0; i < 4; i++) {
+        if (random >= attackThresholds[i][0]) {
+            damage = attackThresholds[i][1];
+            type = attackTypes[i];
+            return;
         }
+    }
+    damage = 2;
+    type = "light";
+}
+void recursiveBattleRound(Player &player, int &enehp, string inventory[]) {
+    int star;
+    cout << "Confirm your attack\n";
+    cout << "How will you attack\n";
+    cout << "1 With strength\n";
+    cout << "2 With intelligence\n";
+    cout << "3 With dexterity\n";
+    cout << "4 with Sword of light can only be used once\n";
+    cin >> star;
+    int random = (rand() % 100) + 1;
+    int damage;
+    string type;
+    getEnemyAttack(random, damage, type);
+    if (star == 2 || star == 3) {
+        if (damage == 2) type = "speical";
+    }
+    switch (star) {
+        case 1:
+            cout << "You have attacked enemy for 5 damage\n";
+            enehp = enehp - 5;
+            if (random >= 75) {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used heavy attack\n";
+                player.health = player.health - 5;
+            } else if (random >= 45) {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used normal attack\n";
+                player.health = player.health - 3;
+            } else if (random >= 92) {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used speical attack\n";
+                player.health = player.health - 10;
+            } else {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used light attack\n";
+                player.health = player.health - 2;
+            }
+            break;
+        case 2:
+            cout << "You have attacked enemy for 3 damage\n";
+            enehp = enehp - 5;
+            if (random >= 75) {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used heavy attack\n";
+                player.health = player.health - 5;
+            } else if (random >= 45) {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used normal attack\n";
+                player.health = player.health - 3;
+            } else if (random >= 92) {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used speical attack\n";
+                player.health = player.health - 10;
+            } else {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used speical attack\n";
+                player.health = player.health - 2;
+            }
+            break;
+        case 3:
+            cout << "You have attacked enemy for 2 damage\n";
+            enehp = enehp - 5;
+            if (random >= 75) {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used heavy attack\n";
+                player.health = player.health - 5;
+            } else if (random >= 45) {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used normal attack\n";
+                player.health = player.health - 3;
+            } else if (random >= 92) {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used speical attack\n";
+                player.health = player.health - 10;
+            } else {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used speical attack\n";
+                player.health = player.health - 2;
+            }
+            break;
+        case 4:
+            cout << "You have used the Sword of Light \n";
+            enehp = enehp - 10;
+            if (random >= 75) {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used heavy attack\n";
+                player.health = player.health - 5;
+            } else if (random >= 45) {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used normal attack\n";
+                player.health = player.health - 3;
+            } else if (random >= 92) {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used speical attack\n";
+                player.health = player.health - 10;
+            } else if (random > 10) {
+                cout << "You have been attacked\n";
+                cout << "Enemy has used light attack\n";
+                player.health = player.health - 2;
+            } else {
+                cout << "You have dodged\n";
+            }
+            break;
+    }
+    if (enehp >= 0 && player.health >= 0) {
+        recursiveBattleRound(player, enehp, inventory);
     }
 }
